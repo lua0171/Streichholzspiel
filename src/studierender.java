81ceeaed50e3a9269ce3739fd9b1a90d87f67069
lua0171 <lua0171@thi.de>
2025-03-21 10:58:26 +0100

    jetzt mit inhalt

--- src/studierender.java
+++ src/studierender.java
@@ -1,3 +1,11 @@
 public class studierender {
+    String name;
+    int matrikelnummer=0;
+    String studiengang;
+    
+    public void frageStellen(){
+        System.out.println("Stellen Sie eine Frage");
+    }
     
 }
+
