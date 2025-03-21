29a87fd757c623f45c0920e7867c2e721819a417
lua0171 <lua0171@thi.de>
2025-03-21 10:47:58 +0100

    Variablen für studierender

--- src/dozierender.java
+++ src/dozierender.java
@@ -1,3 +1,5 @@
 public class dozierender {
-    
+ public static void main(String[] args) {
+     
+ }
 }
--- src/studierender.java
+++ src/studierender.java
@@ -1,3 +1,12 @@
 public class studierender {
+    public static void main(String[] args) {
+        String name;
+        int matrikelnummer=0;
+        String studiengang;
+    }
+    public void frageStellen(){
+        System.out.println("Stellen Sie eine Frage");
+    }
     
 }
+
